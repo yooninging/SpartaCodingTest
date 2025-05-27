5fc604cf7e6d302321fe781abc6a754806554a0d
yooninging <102010872+yooninging@users.noreply.github.com>
2025-05-27 10:51:29 +0900

    [level 0] Title: 두 수의 나눗셈, Time: 0.01 ms, Memory: 4.38 MB -BaekjoonHub

---
+++ 프로그래머스/0/120806. 두 수의 나눗셈/두 수의 나눗셈.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    float temp;
+    
+    answer = (float)num1/(float)num2*1000;
+//    answer = temp*1000;
+    
+    
+    return answer;
+}
