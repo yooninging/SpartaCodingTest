dbc8de23476c65697cd6f78d18066b8afcfe288f
yooninging <102010872+yooninging@users.noreply.github.com>
2025-05-27 10:53:06 +0900

    [level 0] Title: 몫 구하기, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/0/120805. 몫 구하기/몫 구하기.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int num1, int num2) {
+    int answer = 0;
+    int temp;
+    
+    temp = num1 / num2;
+    
+    answer = temp;
+    
+    return answer;
+}
